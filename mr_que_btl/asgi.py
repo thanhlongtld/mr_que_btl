e6052c127e7db0e8bbe6727bbc9ebbf9db09d1e7
Long Do <thanhlongtld@gmail.com>
2022-04-27 23:28:30 +0700

    first push

---
+++ mr_que_btl/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mr_que_btl project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mr_que_btl.settings')
+
+application = get_asgi_application()
