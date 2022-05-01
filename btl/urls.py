a196b98075bee2960445bca986f010f7c7d47f69
dung0312-ptit <dungtn312@gmail.com>
2022-05-01 17:03:07 +0700

    cart, checkout template

--- btl/urls.py
+++ btl/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="test"),
+    path("shop-cart", views.shopcart, name="shop-cart"),
+    path("checkout",views.checkout,name = "checkout"),
 ]
--- btl/views.py
+++ btl/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, "btl/index.html")
+
+
+def shopcart(request):
+    return render(request, "btl/shop-cart.html")
+
+def checkout(request):
+    return render(request,"btl/checkout.html")
